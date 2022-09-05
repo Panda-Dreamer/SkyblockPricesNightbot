dde3da9b6e5319a22f469bab162cb3a3ce45eb77
Omega <castoretpapillon@gmail.com>
2022-09-05 23:34:27 +0200

    e

--- server.js
+++ server.js
@@ -3,29 +3,117 @@ const bodyParser = require("body-parser");
 const url = require("url");
 const querystring = require("querystring");
 const axios = require("axios");
+const { arch } = require("os");
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const items = {};
+var items = {};
+var bzitems = {};
 var last_updated_items = 0;
 var updating = false;
 
+function RequestPage(number) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("https://api.hypixel.net/skyblock/auctions?page=" + number, { headers: { "API-Key": "a29d3e80-9cfc-4e9f-9d31-0dbeb560ca8a" } })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        resolve(false);
+      });
+  });
+}
+
 function updateData() {
-  updating = true;
+
   axios
-    .get("https://api.slothpixel.me/api/skyblock/items")
-    .then((response) => {
-      for (const [id, item] of Object.entries(response.data)) {
-        items[item.name.toLowerCase()] = item;
-        items[item.name.toLowerCase()].id = id;
-        items[item.item_id.toString()] = item;
-        items[item.item_id.toString()].id = id;
+  .get("https://api.hypixel.net/skyblock/bazaar", { headers: { "API-Key": "a29d3e80-9cfc-4e9f-9d31-0dbeb560ca8a" } })
+  .then((response)=>{
+    for (const [product_id, product] of Object.entries(response.data.products)) {
+      bzitems[product.product_id.replaceAll("_","").toLocaleLowerCase().split(":")[0]] = product
+    }
+  })
+  .catch((error) =>{
+    console.log(error)
+  })
+  updating = true;
+  new_items = {};
+  axios
+    .get("https://api.hypixel.net/skyblock/auctions?page=0", { headers: { "API-Key": "a29d3e80-9cfc-4e9f-9d31-0dbeb560ca8a" } })
+    .then(async (response) => {
+      let page_count = response.data.totalPages;
+      console.log("Page count:", page_count);
+      for (var i = 1; i < page_count; i++) {
+        data = await RequestPage(i);
+        console.log(`Processing page n°${i}, Number of auctions: ${data.auctions.length}; Items processed: ${Object.entries(new_items).length}/${response.data.totalAuctions}`);
+        if (data != false && data.success == true) {
+          for (var ai = 0; ai < data.auctions.length; ai++) {
+            let auction = data.auctions[ai];
+            auction.item_name = auction.item_name
+              .replaceAll("✪", "")
+              .replaceAll("✦", "")
+              .replaceAll("➍", "")
+              .replaceAll("⚚", "")
+              .replaceAll("➎", "")
+              .replaceAll("✿", "")
+              .replaceAll("➌", "")
+              .replaceAll("➊", "")
+              .replaceAll("➋", "")
+              .replaceAll("➍", "")
+              .replaceAll("➏", "")
+              .replaceAll("➐", "")
+              .replaceAll("➑", "")
+              .replaceAll("➒", "")
+              .replaceAll("➓", "")
+              .replaceAll("◆", "")
+              .split(" - ")[0]
+              .replace(/\[.*\]/, "")
+              .replace(/\(.*\)/, "");
+            while (auction.item_name.charAt(0) === " ") {
+              auction.item_name = auction.item_name.substring(1);
+            }
+            item = new_items[auction.item_name.toLowerCase()];
+            //process.stdout.write(`Items processed: ${Object.entries(new_items).length}/${response.data.totalAuctions}, Page:${i}/${page_count}                    \r`);
+
+            if (!item) {
+              //console.log(`Adding item:${auction.item_name}`)
+              new_items[auction.item_name.toLowerCase()] = {
+                name: auction.item_name,
+                lowest_starting_bid: auction.starting_bid,
+                lowest_highest_bid: auction.highest_bid_amount || "99999999999999999999999999999999999999999",
+                amount: 1,
+                highest_starting_bid: auction.starting_bid,
+                highest_highest_bid: auction.highest_bid_amount || "0",
+              };
+            } else {
+              let current_item = new_items[auction.item_name.toLowerCase()];
+              new_items[auction.item_name.toLowerCase()].amount = current_item.amount + 1;
+
+              if (current_item.lowest_starting_bid > auction.starting_bid) {
+                new_items[auction.item_name.toLowerCase()].lowest_starting_bid = auction.starting_bid;
+              }
+
+              if (current_item.lowest_highest_bid > auction.highest_bid_amount) {
+                new_items[auction.item_name.toLowerCase()].lowest_highest_bid = auction.highest_bid_amount;
+              }
+
+              if (current_item.highest_starting_bid < auction.starting_bid) {
+                new_items[auction.item_name.toLowerCase()].highest_starting_bid = auction.starting_bid;
+              }
+
+              if (current_item.highest_starting_bid < auction.highest_bid_amount) {
+                new_items[auction.item_name.toLowerCase()].highest_starting_bid = auction.highest_bid_amount;
+              }
+            }
+          }
+        }
       }
-      last_updated_items = new Date();
-      updating = false;
-      console.log(Object.keys(items).length);
+      items = new_items;
+      console.log("Items processed: ", Object.entries(items).length);
+      updating = false
     })
     .catch((error) => {
       console.log(error);
@@ -33,72 +121,68 @@ function updateData() {
     });
 }
 
+function nFormatter(num, digits) {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" }
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup.slice().reverse().find(function(item) {
+    return num >= item.value;
+  });
+  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+}
+
 // Function to handle the root path
 app.get("/api/prices", async function (req, res) {
-  // Access the provided 'page' and 'limt' query parameters
-  // Access the provided 'page' and 'limt' query parameters
-  let query = req.query.q;
-  if (!query) {
-    res.send("Missing query field");
-    return;
-  }
-  let item = query.split(" ").join("_").toUpperCase()
-  if (!item) {
-    res.send("L'object n'existe pas");
-    return;
-  }
-  console.log(item);
+  let query = req.query.q
 
-  axios
-    .get("https://api.slothpixel.me/api/skyblock/bazaar/" + item)
-    .then((response) => {
-      if (response.data.error) {
-        if (response.data.error == "Invalid itemId") {
-          res.send("L'object n'est pas vendu au bazar");
-          return;
-        } else {
-          res.send("Une erreur est survenue");
-          return;
-        }
-      } else {
-        let sum = response.data.quick_status;
-        if (!sum) {
-          res.send(`Impossible d'obtenir les prix pour cet objet`);
-          return;
-        }
-        res.send(`[${query}](Bazar) Achat: ${sum.buyPrice.toString().split(".")[0]}, Vente: ${sum.sellPrice.toString().split(".")[0]}`);
-        return;
-      }
-    })
-    .catch((error) => {
-        axios
-        .get("https://api.slothpixel.me/api/skyblock/auctions?sortBy=starting_bid&id=" + item)
-        .then((response) => {
-            if(response.data.matching_query == 0){
-                res.send(`Aucun item de ce type en vente`);
-                eturn;
-            }
-            axios
-            .get("https://api.slothpixel.me/api/skyblock/auctions?sortBy=highest_bid_amount&sortOrder=asc&id=" + item)
-            .then((response2) => {
-                lowestBid = 9999999999999999999999999999999999999999999999
-                for (const auction of response2.data.auctions) {
-                    if(auction.highest_bid_amount < lowestBid && auction.highest_bid_amount != 0){
-                        lowestBid = auction.highest_bid_amount
-                    }
-                }
-            res.send(`[${query}](AH) En vente: ${response.data.matching_query}, Prix de départ le plus bas: ${response.data.auctions[response.data.auctions.length -1].starting_bid}, Enchère la plus basse: ${lowestBid}`);
-            return;
-            })
-        })
-        .catch((error) => {
-            res.send(`Impossible d'obtenir les prix pour cet objet`);
-            return;
-        });
-    });
+  if(!query){
+    res.send("Utilisation: ![cmd] <nom de l'objet>")
+    return
+  }
+
+  let item = items[query.toLocaleLowerCase()]
+  let bzitem = bzitems[query.toLocaleLowerCase()]
+
+  console.log(query.toLocaleLowerCase(),item)
+  console.log(query.toLocaleLowerCase(),bzitem)
+
+  if((!item) && (!bzitem)){
+    res.send("Pas d'item trouvé avec ce nom")
+    return
+  }
+
+  let bzstring = ``
+  let ahstring = ``
+  let queryString = `[${query}] - `
+  let separator = ""
+
+  if(bzitem){
+    bzstring = ` (BZ): Acheter:${nFormatter(bzitem.quick_status.buyPrice,1)},  Vendre:${nFormatter(bzitem.quick_status.sellPrice,1)}`
+  }
+  
+
+  if(item){
+
+    let lowprice = (item.lowest_highest_bid != 0 && item.lowest_highest_bid  != "99999999999999999999999999999999999999999") ? (item.lowest_highest_bid  == "99999999999999999999999999999999999999999" ? "" : "Enchère la plus basse:" + nFormatter(item.lowest_highest_bid) + ",") : ("Enchère la plus basse:" + nFormatter(item.lowest_starting_bid) + ",")
+    ahstring = ` (AH): ${lowprice} ${item.highest_highest_bid  == "0" ? "" : "Enchère la plus haute:" + nFormatter(item.highest_highest_bid) +";"} Nombre d'enchères: ${nFormatter(item.amount,1)}`
+  }
+
+  if(ahstring!= "" && bzstring != ""){
+    separator = "||"
+  }
+
+  finalString = queryString + bzstring + separator +ahstring
+  res.send(finalString)
 });
-
 
 let server = app.listen(8000, function () {
   console.log("Server is listening on port 8000");
+  updateData();
 });
